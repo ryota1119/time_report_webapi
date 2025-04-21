15fb9954bf7333355bbb6a7b61e79f50aaa14d50
篠原亮太 <>
2025-04-21 21:17:47 +0900

    test commit

---
+++ internal/helper/time.go
@@ -0,0 +1,13 @@
+package helper
+
+import "time"
+
+func EqualTimePtr(a, b *time.Time) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil || b == nil {
+		return false
+	}
+	return a.Equal(*b)
+}
---
+++ internal/usecase/timer_stop_usecase.go
@@ -0,0 +1,73 @@
+package usecase
+
+import (
+	"context"
+	"database/sql"
+	"github.com/ryota1119/time_resport/internal/domain/repository"
+	"time"
+
+	"github.com/ryota1119/time_resport/internal/domain/entities"
+)
+
+var _ TimerStopUsecase = (*timerStopUsecase)(nil)
+
+// TimerStopUsecase TimerUsecaseのインターフェースを定義
+type TimerStopUsecase interface {
+	// Stop は予算を新規作成する
+	Stop(ctx context.Context, input TimerStopUsecaseInput) (*entities.Timer, error)
+}
+
+// timerStopUsecase ユースケース
+type timerStopUsecase struct {
+	db        *sql.DB
+	timerRepo repository.TimerRepository
+}
+
+func NewTimerStopUsecase(
+	db *sql.DB,
+	timerRepo repository.TimerRepository,
+) TimerStopUsecase {
+	return &timerStopUsecase{
+		db:        db,
+		timerRepo: timerRepo,
+	}
+}
+
+// TimerStopUsecaseInput TimerStopUsecase Stopメソッド用input
+type TimerStopUsecaseInput struct {
+	TimerID uint
+}
+
+// Stop は予算を新規作成する
+func (a *timerStopUsecase) Stop(ctx context.Context, input TimerStopUsecaseInput) (*entities.Timer, error) {
+	tx, err := a.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	timerID := entities.TimerID(input.TimerID)
+	timer, err := a.timerRepo.Find(ctx, tx, &timerID)
+	if err != nil {
+		return nil, err
+	}
+
+	if timer.EndAt != nil {
+		return nil, entities.ErrTimerAlreadyStopped
+	}
+
+	now := time.Now()
+	timer.EndAt = entities.TimerEndAtOrNil(&now)
+	_, err = a.timerRepo.Update(ctx, tx, timer)
+	if err != nil {
+		return nil, err
+	}
+
+	return timer, nil
+}
